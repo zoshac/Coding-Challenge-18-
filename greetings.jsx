c652789039be7ae58bc46f24b228bf2e5eae0e4d
Zosha Chung <czosha@gmail.com>
2025-04-06 23:27:53 -0400

    Project set up

---
+++ greetings.jsx
@@ -0,0 +1,11 @@
+// Task 1 Creating a new Component 
+function Greetings() {
+    return (
+        <div>
+            <h2>Hello from the Greeting Component!</h2>
+            <p>This is your first custom component in React.</p>
+        </div>
+    );
+}
+
+export default Greetings; // greetings component 
